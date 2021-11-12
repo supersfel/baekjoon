decf6fb307e736059107c1600c333c4e83d7fd50
supersfel <jmg081005@naver.com>
2021-11-13 01:11:48 +0900

    2581

---
+++ math_problem/2581_find_sosu.py
@@ -0,0 +1,22 @@
+m = int(input())
+n = int(input())
+lst=[]
+
+for i in range(m,n+1):
+
+    f = True
+    if i<=1: continue
+
+    for j in range(2,i):
+        if i%j==0:
+            f=False
+            break
+    if f:
+        lst.append(i)
+
+if len(lst)==0 :
+    print(-1)
+else :
+    print(sum(lst))
+    print(min(lst))
+
