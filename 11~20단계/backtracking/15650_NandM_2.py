744a3afe08449b134b7dce02ace4be722530cecc
supersfel <jmg081005@naver.com>
2021-12-10 21:52:31 +0900

    15650
    15650

--- 11~20단계/backtracking/15650_NandM_2.py
+++ 11~20단계/backtracking/15650_NandM_2.py
@@ -1,18 +1,15 @@
 N,M = map(int,input().split())
 
-lst=[]
-
+lst = []
 def f():
     if len(lst) == M:
         print(' '.join(map(str,lst)))
         return
 
-    for i in range(1,N+1):
-        if i not in lst:
+    for i in range(1, N + 1):
+        if  len(lst) == 0 or i > lst[-1] :
             lst.append(i)
             f()
             lst.pop()
 
 f()
-
-
