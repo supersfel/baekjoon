28b1c8be263df2e7afc73d275c0be2bec2d00913
supersfel <jmg081005@naver.com>
2021-12-27 22:30:15 +0900

    2579

--- 11~20단계/동적계획법/2579_계단오르기.py
+++ 11~20단계/동적계획법/2579_계단오르기.py
@@ -1,6 +1,16 @@
 N = int(input())
-stair = [ [int(input()),0] for _ in range(N)]
+stair = []
+for _ in range(N):
+    stair.append(int(input()))
+result = []
 
-print(stair)
 
-for i in range(1,len(stair)):
+
+result.append(stair[0])
+result.append(max(stair[0]+stair[1],stair[1]))
+result.append(max(stair[0]+stair[2],stair[1]+stair[2]))
+
+for i in range(3,N):
+    result.append(max(result[i-2],result[i-3] + stair[i-1])+stair[i])
+
+print(result[-1])
