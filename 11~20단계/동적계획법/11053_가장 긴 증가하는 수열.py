1deb76c9d32fa31a33af60d88b5eb88db24421b5
supersfel <jmg081005@naver.com>
2021-12-31 15:36:16 +0900

    11053

---
+++ 11~20단계/동적계획법/11053_가장 긴 증가하는 수열.py
@@ -0,0 +1,12 @@
+N = int(input())
+lst = list(map(int,input().split()))
+result = [ 0 for _ in range(N) ]
+
+for i in range(N):
+    for j in range(i):
+        if lst[i] > lst[j] and result[i] < result[j]:
+            result[i] = result[j]
+    result[i] +=1
+
+
+print(max(result))
