745bd658bce8bbe8b711094eaa844dbe32d520b9
supersfel <81850778+supersfel@users.noreply.github.com>
2022-01-05 17:17:09 +0900

    Create 9251_LCS.py

---
+++ 11~20단계/동적계획법/9251_LCS.py
@@ -0,0 +1,18 @@
+import sys
+
+a = sys.stdin.readline().strip().upper()
+b = sys.stdin.readline().strip().upper()
+
+
+result = [[0] * (len(b)+1) for i in range(len(a)+1)]
+
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+        if a[i-1] == b[j-1]:
+            result[i][j] = result[i-1][j-1] + 1
+        else:
+            result[i][j] = max(result[i-1][j],result[i][j-1])
+
+for i in result:
+    print(i)
+print(result[-1][-1])
