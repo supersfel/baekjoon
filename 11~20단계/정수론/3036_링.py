9fea71e41e0287d8879cf12149b186b104053285
supersfel <jmg081005@naver.com>
2022-01-26 00:02:33 +0900

    11050_이항계수

--- 11~20단계/정수론/3036_링.py
+++ 11~20단계/정수론/3036_링.py
@@ -6,3 +6,16 @@ first = lst[0]
 for i in range(1,n):
     num=Fraction(first,lst[i])
     print(num.numerator,'/',num.denominator,sep='')
+
+
+# a = int(input())
+# ring = list(map(int, input().split()))
+# for i in ring:
+#     if i != ring[0]:
+#         a, b = ring[0],i
+#         a_c,b_c = a,b
+#         while True:
+#             a, b = b, a%b
+#             if b==0:
+#                 break
+#         print('%d/%d'%(int(a_c/a),int(b_c/a)))
