c5fb4148334d27abb340e04166c3fd4e61c030b1
supersfel <jmg081005@naver.com>
2022-01-27 21:10:07 +0900

    1010_다리놓기

---
+++ 11~20단계/정수론/1010_다리놓기.py
@@ -0,0 +1,12 @@
+t = int(input())
+fac =[1]
+for i in range(1,31):
+    fac.append(fac[-1] * i)
+
+for _ in range(t):
+    n,m = map(int,input().split())
+    print( fac[m] // (fac[n] * fac[m-n]) )
+
+
+
+
--- 11~20단계/정수론/11050_이항계수1.py
+++ 11~20단계/정수론/11050_이항계수1.py
@@ -3,4 +3,4 @@ lst=[1,1]
 for a in range(2,11):
     lst.append(lst[-1]*a)
 
-print( lst[n] // ( lst[k] * (lst[n-k]) ) )
+print( lst[n] // ( lst[k] * (lst[n-k])))
