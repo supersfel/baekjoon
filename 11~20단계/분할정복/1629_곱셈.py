02c2a60d10f4c3905ca418c16d0ea7e1d8ff6e3c
supersfel <jmg081005@naver.com>
2022-02-22 22:16:20 +0900

    1629_곱셈

---
+++ 11~20단계/분할정복/1629_곱셈.py
@@ -0,0 +1,14 @@
+a,b,c = map(int,input().split())
+
+def f(a,b):
+    if b ==1:
+        return a%c
+
+    num = f(a,b//2)
+
+    if b%2 ==0:
+        return num * num%c
+    else:
+        return num * num*a%c
+
+print(f(a,b,))
