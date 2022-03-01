dba5a24b5d757f2ad2f3f799256da535750c526a
supersfel <jmg081005@naver.com>
2022-03-01 11:55:20 +0900

    이분탐색

---
+++ 21~30단계/이분탐색/10816_숫자카드2.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline())
+lst = list(map(int,sys.stdin.readline().split()))
+m = sys.stdin.readline()
+find_lst = list(map(int,sys.stdin.readline().split()))
+
+count ={}
+for i in lst:
+    try : count[i] +=1
+    except : count[i]=1
+
+for i in find_lst:
+    try : print(count[i],end= ' ')
+    except : print('0',end= ' ')
