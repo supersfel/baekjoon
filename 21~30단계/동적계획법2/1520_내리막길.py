18c9eaa60f49aef1e915cb0fa35743620b24b8a8
supersfel <81850778+supersfel@users.noreply.github.com>
2022-03-15 20:21:37 +0900

    Update 1520_내리막길.py

--- 21~30단계/동적계획법2/1520_내리막길.py
+++ 21~30단계/동적계획법2/1520_내리막길.py
@@ -1,4 +1,35 @@
 i,j = map(int,input().split())
 map = [ list(map(int,input().split())) for _ in range(i) ]
 
-print(map)
+# print(map)
+visited = [ [ -1 for _ in range(j)] for __ in range(i)]
+# print(visited)
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+
+def f(x,y):
+    for k in visited:
+        print(k)
+    print('-------------------')
+    if x == i-1 and y == j-1:
+        return 1
+    if visited[x][y] != -1:
+        return visited[x][y]
+
+    visited[x][y] = 0
+
+    for q in range(4):
+        nx = x + dx[q]
+        ny = y + dy[q]
+
+        if 0<= nx < i and 0 <= ny < j:
+            if map[nx][ny] < map[x][y]:
+                visited[x][y] += f(nx,ny)
+
+
+    return visited[x][y]
+
+
+print(f(0,0))
+
