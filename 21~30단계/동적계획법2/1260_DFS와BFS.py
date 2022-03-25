abe7c3b0b6a89513c6b91ea8fc26663d08d7e14e
supersfel <jmg081005@naver.com>
2022-03-25 23:51:34 +0900

    Create 1260_DFS와BFS.py

---
+++ 21~30단계/동적계획법2/1260_DFS와BFS.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+n,m,start=map(int,input().split())
+visited=[False]*(n+1)
+
+graph=[[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(len(graph)):
+    graph[i].sort()
+
+
+def dfs(v):
+    visited[v] = True
+    print(v, end = ' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+            visited[i]=True
+
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in graph[v]:
+
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+dfs(start)
+print()
+visited = [False]*(n+1)
+bfs(start)
+
