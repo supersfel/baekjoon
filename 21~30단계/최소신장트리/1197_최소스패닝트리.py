a62b0d6b262a01850642d7f079a58c4a6f0067f1
supersfel <81850778+supersfel@users.noreply.github.com>
2022-05-11 11:59:43 +0900

    Create 1197_최소스패닝트리.py

---
+++ 21~30단계/최소신장트리/1197_최소스패닝트리.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find(node):
+    if parents[node] == node:
+        return node
+    parents[node] = find(parents[node])
+    return parents[node]
+
+def union(a,b):
+    a,b = find(a),find(b)
+    if a> b:
+        parents[a] = b
+    else:
+        parents[b] = a
+
+v,e = map(int,input().split())
+parents = [ i for i in range(v+1)]
+way = []
+for i in range(e):
+    a,b,c = map(int,input().split())
+    way.append((a,b,c))
+
+way.sort(key=lambda x : x[2])
+result = 0
+for a,b,c in way:
+    if find(a) != find(b):
+        union(a,b)
+        result += c
+print(result)
+
+
