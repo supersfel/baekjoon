7c7e2f00a4f0c17ff4bbf1ecaa031473af748fe0
supersfel <jmg081005@naver.com>
2022-05-19 18:57:54 +0900

    Create 1949_우수마을.py

---
+++ 31~40단계/트리에서의 동적계획법/1949_우수마을.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+n = int(input())
+population = [0]+list(map(int,input().split()))
+
+tree = [ [] for _ in range(n+1) ]
+for _ in range(n-1):
+    a,b = map(int,input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+visited= [False] * (n+1)
+dp = [ [0,0] for _ in range(n+1) ]
+
+def dfs(node):
+    visited[node] = 1
+    dp[node][1] = population[node]
+
+    for i in tree[node]:
+        if not visited[i]:
+            dfs(i)
+            dp[node][1] += dp[i][0]
+            dp[node][0] += max(dp[i][0],dp[i][1])
+
+
+dfs(1)
+print(max(dp[1][0],dp[1][1]))
