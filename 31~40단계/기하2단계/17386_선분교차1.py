94d43d03ee71c85da3cc463326d348dc65410219
supersfel <81850778+supersfel@users.noreply.github.com>
2022-05-24 22:02:29 +0900

    선분교차

--- 31~40단계/기하2단계/17386_선분교차1.py
+++ 31~40단계/기하2단계/17386_선분교차1.py
@@ -11,8 +11,11 @@ def ccw(x1,y1,x2,y2,x3,y3):
         return -1
     else:
         return 0
-
-if (ccw(x1,y1,x2,y2,x3,y3) * ccw(x1,y1,x2,y2,x4,y4))<=0 and (ccw(x3,y3,x4,y4,x1,y1)*ccw(x3,y3,x4,y4,x2,y2))<=0:
-    print(1)
-else:
-    print(0)
+answer=0
+if (ccw(x1,y1,x2,y2,x3,y3) * ccw(x1,y1,x2,y2,x4,y4))==0 and (ccw(x3,y3,x4,y4,x1,y1)*ccw(x3,y3,x4,y4,x2,y2))==0:
+    if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(
+        y1, y2):
+        answer  = 1
+elif (ccw(x1,y1,x2,y2,x3,y3) * ccw(x1,y1,x2,y2,x4,y4))<=0 and (ccw(x3,y3,x4,y4,x1,y1)*ccw(x3,y3,x4,y4,x2,y2))<=0:
+    answer = 1
+print(answer)
