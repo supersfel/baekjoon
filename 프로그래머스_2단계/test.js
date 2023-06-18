f008540611ba4f2992fe67699c2117305e763302
정민규 <jmg081005@naver.com>
2023-06-18 18:36:18 +0900

    Update test.js

--- 프로그래머스_2단계/test.js
+++ 프로그래머스_2단계/test.js
@@ -10,7 +10,7 @@ ary6.length = 3;
 const ary7 = Array.from({ length: 3 }).map((v, i) => i);
 
 console.log(ary1); //(3) [...]
-console.log(ary2); //(3) [...]
+console.log(ary2); //(3) [0,1,2]
 console.log(ary3); // [undefined,undefined,undefined]
 console.log(ary4); // [undefined,undefined,undefined]
 console.log(ary5); //(3) [...]
@@ -20,3 +20,4 @@ console.log(ary7); // [undefined,undefined,undefined]
 for (const el of ary2) {
   console.log(el);
 }
+
