55946ff9f79015d75cd381b595b39127e0968640
정민규 <jmg081005@naver.com>
2023-06-25 21:46:52 +0900

    Update test.js

--- 프로그래머스_2단계/test.js
+++ 프로그래머스_2단계/test.js
@@ -1,23 +1,7 @@
-const ary1 = new Array(3);
-const ary2 = Array(3)
-  .fill(0)
-  .map((_, i) => i);
-const ary3 = [...new Array(3)];
-const ary4 = [...Array(3)];
-const ary5 = [, , ,];
-const ary6 = [];
-ary6.length = 3;
-const ary7 = Array.from({ length: 3 }).map((v, i) => i);
+const ary = new Array(10).fill().map((_) => [false, false, false, false]);
 
-console.log(ary1); //(3) [...]
-console.log(ary2); //(3) [0,1,2]
-console.log(ary3); // [undefined,undefined,undefined]
-console.log(ary4); // [undefined,undefined,undefined]
-console.log(ary5); //(3) [...]
-console.log(ary6); //(3) [...]
-console.log(ary7); // [undefined,undefined,undefined]
+console.log(ary);
 
-for (const el of ary2) {
-  console.log(el);
-}
+ary[0][2] = true;
 
+console.log(ary);
